fc59a99c91c83910a8bf87db2c1bfbd37794d79d
zeofr <157026468+zeofr@users.noreply.github.com>
2026-02-16 10:14:59 +0530

    Fix expected result in test_add for add function
    Corrected expected result for add function test.

--- test_calculator.py
+++ test_calculator.py
@@ -2,7 +2,7 @@ import pytest
 from calculator import add, subtract, multiply, divide
 
 def test_add():
-    assert add(2, 3) == 5
+    assert add(2, 3) == 10
     assert add(-1, 1) == 0
 
 def test_subtract():
