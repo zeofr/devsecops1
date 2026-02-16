c9f54dffbf79252f7389124a071cc2b8a745d1b2
zeofr <157026468+zeofr@users.noreply.github.com>
2026-02-16 09:47:28 +0530

    Add test for the add function in calc module

---
+++ test_calc.py
@@ -0,0 +1,11 @@
+# test_calc.py
+from calc import add
+
+def test_add():
+    # This is the logic check
+    assert add(2, 3) == 5# test_calc.py
+from calc import add
+
+def test_add():
+    # This is the logic check
+    assert add(2, 3) == 5
